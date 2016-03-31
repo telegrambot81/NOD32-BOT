1951b3430bd659e03531028ca0280b6d89fa2266
telegrambot81 <telegrambot81@gmail.com>
2016-03-31 18:17:54 +0430

    Update Auto_Leave.lua

--- plugins/Auto_Leave.lua
+++ plugins/Auto_Leave.lua
@@ -1,8 +1,8 @@
 do
 -- Will leave the group if be added
 local function run(msg, matches)
-local bot_id = our_id -- your bot id
-   -- like local bot_id = 1234567
+local bot_id = 120629055
+   -- like local bot_id = 194647263
     if matches[1] == 'leave' and is_admin(msg) then
        chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
     elseif msg.action.type == "chat_add_user" and msg.action.user.id == tonumber(bot_id) and not is_sudo(msg) then
