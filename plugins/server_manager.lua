46614b6d2c96dde07e1bcec347665c13e376c347
telegrambot81 <telegrambot81@gmail.com>
2016-04-07 20:57:54 +0430

    Update server_manager.lua

--- plugins/server_manager.lua
+++ plugins/server_manager.lua
@@ -2,7 +2,7 @@ do
     
 function run(msg, matches)
   if matches[1] == "reboot" and is_sudo(msg) then
-        local s = io.popen("sh /home/TeleSeed/data/com/upstart.sh") 
+        local s = io.popen("sh /home/NOD32-BOT/data/com/upstart.sh") 
         return ( s:read("*a") ) 
   elseif matches[1] == "serverinfo" and is_sudo(msg) then
      local f = io.popen("sh /home/TeleSeed/data/com/cmd.sh") 
